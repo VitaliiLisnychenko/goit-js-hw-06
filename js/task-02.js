a292ddde1ceec1ec472a0200bebf0819541a9fa9
VitaliiLisnychenko <lisnychenkovitalii@gmail.com>
2023-07-14 16:40:47 +0200

    tesk 1-2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngr = document.getElementById("ingredients");
+
+const itemsIngr = ingredients.map((ingredient) => {
+  const ingredientEl = document.createElement("li");
+  ingredientEl.classList.add("item");
+  ingredientEl.textContent = ingredient;
+  return ingredientEl;
+})
+
+listIngr.append(...itemsIngr);
