14ec395409769cc1b1cdcd881868dced5b57eea9
VitaliiLisnychenko <lisnychenkovitalii@gmail.com>
2023-07-17 17:03:46 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -4,13 +4,13 @@ const btnEl = document.querySelector(".change-color");
 const spanEl = document.querySelector(".color");
 const bodyEl = document.querySelector("body");
 
+btnEl.addEventListener("click", onBtnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
-
-btnEl.addEventListener("click", onBtnClick);
 
 function onBtnClick(evt) {
   bodyEl.style.backgroundColor = getRandomHexColor();
